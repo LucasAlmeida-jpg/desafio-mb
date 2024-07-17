a59bac75419cf3fadbdcb278c263554c2f47c13a
Lucas Almeida <lucasalmeidagouvea123@gmail.com>
2024-07-16 22:10:12 -0300

    tests

---
+++ src/components/__tests__/Register.spec.js
@@ -0,0 +1,56 @@
+import { mount } from '@vue/test-utils';
+import Register from '@/components/Register.vue';
+import FormStep1 from '@/components/FormStep1.vue';
+import FormStep2 from '@/components/FormStep2.vue';
+import FormStep3 from '@/components/FormStep3.vue';
+import FormStep4 from '@/components/FormStep4.vue';
+
+describe('Register.vue', () => {
+  it('renders correct step component based on currentStep', async () => {
+    const wrapper = mount(Register);
+
+    expect(wrapper.findComponent(FormStep1).exists()).toBe(true);
+    expect(wrapper.findComponent(FormStep2).exists()).toBe(false);
+    expect(wrapper.findComponent(FormStep3).exists()).toBe(false);
+    expect(wrapper.findComponent(FormStep4).exists()).toBe(false);
+
+    await wrapper.vm.handleStepValidation({ email: 'test@example.com' });
+
+    expect(wrapper.findComponent(FormStep1).exists()).toBe(false);
+    expect(wrapper.findComponent(FormStep2).exists()).toBe(true);
+    expect(wrapper.findComponent(FormStep3).exists()).toBe(false);
+    expect(wrapper.findComponent(FormStep4).exists()).toBe(false);
+
+    await wrapper.vm.handleStepValidation({ registrationType: 'PJ' });
+
+    expect(wrapper.findComponent(FormStep2).exists()).toBe(false);
+    expect(wrapper.findComponent(FormStep3).exists()).toBe(true);
+    expect(wrapper.findComponent(FormStep4).exists()).toBe(false);
+
+    await wrapper.vm.handleStepValidation({ companyName: 'Company XYZ' });
+
+    expect(wrapper.findComponent(FormStep3).exists()).toBe(false);
+    expect(wrapper.findComponent(FormStep4).exists()).toBe(true);
+
+    await wrapper.vm.submitForm();
+
+    await new Promise(resolve => setTimeout(resolve, 3000));
+
+  });
+
+  it('allows user to go back to previous steps', async () => {
+    const wrapper = mount(Register);
+
+    await wrapper.vm.handleStepValidation({ email: 'test@example.com' });
+
+    await wrapper.vm.handleStepValidation({ registrationType: 'PJ' });
+
+    await wrapper.vm.goToPreviousStep();
+
+    expect(wrapper.findComponent(FormStep2).exists()).toBe(true);
+
+    await wrapper.vm.goToPreviousStep();
+
+    expect(wrapper.findComponent(FormStep1).exists()).toBe(true);
+  });
+});
---
+++ src/components/__tests__/StepCounter.spec.js
@@ -0,0 +1,13 @@
+import { shallowMount } from '@vue/test-utils';
+import StepCounter from '@/components/StepCounter.vue';
+
+describe('StepCounter.vue', () => {
+  it('renders props.currentStep when passed', () => {
+    const currentStep = 2;
+    const totalSteps = 5;
+    const wrapper = shallowMount(StepCounter, {
+      propsData: { currentStep, totalSteps }
+    });
+    expect(wrapper.text()).toContain(`Etapa ${currentStep} de ${totalSteps}`);
+  });
+});
